f887e786602e44af8562d646373de85582a31dbb
Arefin Aareef <arefin.aareef@gmail.com>
2023-06-02 15:24:51 +0600

    small changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sypbrfe.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,14 @@ async function run() {
         res.send({totalProducts: result})
     })
 
+    app.post('/productsByIds', async(req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map(id => new ObjectId(id))
+      const query = { _id: { $in: objectIds } }
+      console.log(ids);
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
+    })
 
 
 
